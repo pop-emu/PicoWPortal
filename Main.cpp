2230f6bd7c390a46f89d3c90977f7438a4a2f619
Marijn Kneppers <marijnkneppers@gmail.com>
2023-02-11 14:48:55 +0100

    Removed the wifi connection until it's required Couldn't get a VNC connection at school and therefor couldn't debug. Pico didn't want to connect to the school network.

--- Main.cpp
+++ Main.cpp
@@ -14,22 +14,6 @@ int main()
         return -1;
     }
 
-    cyw43_arch_enable_sta_mode();
-
-    int connected;
-
-    do
-    {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
-
-        connected = cyw43_arch_wifi_connect_timeout_ms(WIFI_SSID, WIFI_PASSWORD, CYW43_AUTH_WPA2_AES_PSK, 30000);
-
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
-
-        sleep_ms(500);
-    }
-    while(connected);
-
     tusb_init();
 
     while(true)
